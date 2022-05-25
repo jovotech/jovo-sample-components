b5ae89537f7db5b743ce6284dcfff2c450d88e95
Jan König <jankoenig120190@googlemail.com>
2022-05-25 11:55:49 +0200

    🚧 Add NLP.js workaround

--- src/app.dev.ts
+++ src/app.dev.ts
@@ -1,6 +1,10 @@
 import { app } from './app';
 import { FileDb } from '@jovotech/db-filedb';
 import { JovoDebugger } from '@jovotech/plugin-debugger';
+import { Nlp, NlpjsNlu } from '@jovotech/nlu-nlpjs';
+import { LangEn } from '@nlpjs/lang-en';
+import { Platform } from '@jovotech/framework';
+const { BuiltinDefault } = require('@nlpjs/builtin-default');
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +20,20 @@ app.configure({
     new FileDb({
       pathToFile: '../db/db.json',
     }),
-    new JovoDebugger(),
+    new JovoDebugger({
+      nlu: new NlpjsNlu({
+        languageMap: {
+          en: LangEn,
+        },
+        useModel: true,
+        setupModelCallback: async (parent: Platform, nlp: Nlp) => {
+          nlp.forceNER = true;
+          nlp.container.use(BuiltinDefault, 'extract-builtin');
+          nlp.use(LangEn);
+          nlp.addCorpus('../models');
+        },
+      }),
+    }),
   ],
 });
 
