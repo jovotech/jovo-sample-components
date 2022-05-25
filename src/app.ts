1f8bf84d6ca71fd81adafc38e1065139e2143ff3
Jan König <jankoenig120190@googlemail.com>
2022-05-25 14:43:12 +0200

    🔥 Remove LoveHatePizzaComponent

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import { App } from '@jovotech/framework';
 
 import { GlobalComponent } from './components/GlobalComponent';
-import { LoveHatePizzaComponent } from './components/LoveHatePizzaComponent';
 import { TableReservationComponent } from './components/TableReservationComponent/TableReservationComponent';
 
 /*
@@ -23,7 +22,7 @@ const app = new App({
   | Learn more here: www.jovo.tech/docs/components
   |
   */
-  components: [GlobalComponent, LoveHatePizzaComponent, TableReservationComponent],
+  components: [GlobalComponent, TableReservationComponent],
 
   /*
   |--------------------------------------------------------------------------
